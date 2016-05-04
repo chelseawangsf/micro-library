8cbede8ad11bbe588b9f08ad00fa18f6264f7c75
Chelsea Wang <chelsea.wang@itadmins-MacBook-Pro-2.local>
2016-05-04 11:56:52 -0700

    Adding the library

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var uniqueRandomArray = require('unique-random-array');
+var starWarsNames = require('./starwars-names.json');
+
+
+/*
+module.exports = {
+	all: starWarsNames,
+	random: uniqueRandomArray(starWarsNames)
+};
+*/
+
+exports.all = starWarsNames;
+exports.random = uniqueRandomArray(starWarsNames);
